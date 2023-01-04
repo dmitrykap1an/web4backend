a25e803ea6175527bcb3a619a0d9a02480066c3d
dmitrykap1an <dk475641@gmail.com>
2023-01-04 03:56:42 +0300

    added new dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.0"
 	kotlin("jvm") version "1.7.21"
 	kotlin("plugin.spring") version "1.7.21"
+	id("org.jetbrains.kotlin.plugin.allopen") version "1.7.10"
+	kotlin("plugin.serialization") version "1.7.20"
 }
 
 group = "com.spring"
@@ -15,17 +17,25 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 repositories {
 	mavenCentral()
 }
+allOpen {
+	annotation("jakarta.persistence.Entity")
+}
 
 dependencies {
 	//implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.security:spring-security-web:6.0.1")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.1")
+	implementation("org.postgresql:postgresql:42.5.1")
+	implementation("io.jsonwebtoken:jjwt:0.9.1")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 }
 
 tasks.withType<KotlinCompile> {
